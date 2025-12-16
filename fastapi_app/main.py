455120a70ae08d5693aa79d615e4303515dd3dab
yarramallamaheshbabu <yarramallamaheshbabu@thestackly.com>
2025-12-16 23:30:23 +0530

    Update main.py

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -279,7 +279,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://18.119.210.2"
+        "http://3.133.64.23"
     ],
     allow_credentials=True,
     allow_methods=["*"],
