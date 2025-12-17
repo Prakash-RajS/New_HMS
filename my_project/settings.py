93d68ff6230909791ba906dfe10813ef4690e23a
prakashraj-SP <prakashraj@thestackly.com>
2025-12-17 16:17:09 +0530

    Initial commit / updated project files

--- my_project/settings.py
+++ my_project/settings.py
@@ -75,6 +75,18 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "HMS_project", 
+#         "USER": "root",
+#         "PASSWORD": "Welcome#1234",
+#         "HOST": "127.0.0.1",
+#         "PORT": "3306",
+#         "OPTIONS": { "charset": "utf8mb4" },
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
