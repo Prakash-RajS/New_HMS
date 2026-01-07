bbf88fa6aac658a19afd5eda361e3a62c437bcc3
prakashraj-SP <prakashraj@thestackly.com>
2026-01-07 15:53:20 +0530

    commit

--- hms_frontend/src/components/Header.jsx
+++ hms_frontend/src/components/Header.jsx
@@ -304,10 +304,10 @@ const API_BASE = import.meta.env.VITE_API_BASE_URL;
       <header
         className="flex items-center justify-between p-4 bg-gray-100 dark:bg-black text-black dark:text-white dark:border-[#1E1E1E] gap-[20px] transition-all duration-300 ease-in-out"
         style={{
-          width: `calc(100% - ${isCollapsed ? "120px" : "220px"})`,
+          width: `calc(100% - ${isCollapsed ? "70px" : "220px"})`,
           maxWidth: "2580px",
           position: "fixed",
-          left: isCollapsed ? "100px" : "247px",
+          left: isCollapsed ? "100px" : "267px",
           zIndex: 40,
           transitionProperty: "width, left",
           transitionDuration: "300ms",
